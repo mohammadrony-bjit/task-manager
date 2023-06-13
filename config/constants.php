4259f1bc6d33a2f8ac061509637a1be6726517f9
Md. Ashadousjaman Rony <ashadousjaman.rony@bjitacademy.com>
2023-06-13 12:19:07 +0600

    Update site url name.

--- config/constants.php
+++ config/constants.php
@@ -6,5 +6,5 @@ define('DB_SERVER', 'task-manager-db.casgqgkkhwig.ap-southeast-2.rds.amazonaws.c
 define('DB_USERNAME', 'rony');
 define('DB_PASSWORD', '12345678');
 define('DB_NAME', 'task_manager');
-define('SITEURL', 'http://rony.sudip.site/');
+define('SITEURL', 'https://rony.sudip.site/');
 ?>
