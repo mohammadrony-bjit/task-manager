844077e24778b16050f41112a0b3cf131f9d27ae
Md. Ashadousjaman Rony <ashadousjaman.rony@bjitacademy.com>
2023-06-22 16:08:52 +0600

    Update project.

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php 
+//Start Session
+session_start();
+//Create Constants to save Database Credentials
+define('DB_SERVER', 'task-mysql-service');
+define('DB_USERNAME', 'rony');
+define('DB_PASSWORD', '12345678');
+define('DB_NAME', 'task_manager');
+define('SITEURL', 'http://192.168.199.18:30000');
+?>
